cd5df6eb90d53e813dd5bef8cd88863b0969abba
Arda Yigithan Orhan <yigithan@kolektiflabs.com>
2018-08-30 00:34:05 +0300

    implemented public api

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var Client = require('./lib/client.js');
+const Client = require('./lib/client.js');
 
 function createClient (options) {
-    return new Client(options)
+    if (typeof options !== 'object'){
+        throw 'Invalid options format. Parameter options must be an object';
+    } else if (!options.endpoint){
+        throw 'Missing parameter in options: endpoint'
+    } else {
+        return new Client(options);
+    }
 }
 
 module.exports = {
